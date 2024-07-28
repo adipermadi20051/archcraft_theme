fd4c41170150d60aad2fd57df610775acfdc9564
dprmd <adipr671@gmail.com>
2024-07-28 08:03:22 +0700

    initial commit

---
+++ theme.bash
@@ -0,0 +1,96 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# dprmd Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#03346E'
+foreground='#FFFFFF'
+color0='#000000'
+color1='#fc0b03'
+color2='#28fc03'
+color3='#2039c7'
+color4='#2140ed'
+color5='#4763ff'
+color6='#0329fc'
+color7='#a3b5b8'
+color8='#2039c7'
+color9='#0972eb'
+color10='#021194'
+color11='#0349a4'
+color12='#045fb5'
+color13='#053fc5'
+color14='#06ccd7'
+color15='#a3b5b8'
+
+accent='#0972eb'
+color_red='#f04d4d'
+color_green='#0fa7c5'
+color_yellow='#ffff00'
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='0xProto Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='0xProto Nerd Font 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='0xProto Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='dprmd.conf'
+geany_font='0xProto Nerd Fonts'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Cyberpunk'
+icon_theme='Archcraft-Dark'
+cursor_theme='LyraP'
+
+# Openbox
+ob_theme='Mikachu'
+ob_layout='LIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-simple.xml'
+ob_margin_t='0'
+ob_margin_b='0'
+ob_margin_r='0'
+ob_margin_l='0'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x36'
+dunst_origin='top-right'
+dunst_font='0xProto Nerd Font 10'
+dunst_border='1'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='0'
+plank_position='right'
+plank_theme='Transparent'
+plank_icon_size='40'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
