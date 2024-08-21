4352a7d9c18ff394b958c3696afbc42998b8814b
dprmd <adipr671@gmail.com>
2024-08-21 23:43:36 +0700

    fix number in GB and MB

--- dprmd/polybar/scripts/data_usage.sh
+++ dprmd/polybar/scripts/data_usage.sh
@@ -7,8 +7,24 @@ interface="wlan0"
 rx_bytes=$(cat /sys/class/net/$interface/statistics/rx_bytes)
 tx_bytes=$(cat /sys/class/net/$interface/statistics/tx_bytes)
 
-# Menghitung total data yang terpakai dalam Megabytes
-total_bytes=$((($rx_bytes + $tx_bytes) / 1024 / 1024))
+# Menghitung total data yang terpakai dalam Bytes
+total_bytes=$(($rx_bytes + $tx_bytes))
 
-# Menampilkan total data yang terpakai
-echo "$total_bytes MB"
+# Menghitung total data dalam MB
+total_mb=$(($total_bytes / 1048576))
+
+if [ $total_mb -lt 1024 ]; then
+  # Jika total data kurang dari 1024 MB, tampilkan dalam MB
+  echo "$total_mb MB"
+else
+  # Jika total data lebih dari atau sama dengan 1024 MB, tampilkan dalam GB
+  total_gb=$(echo "scale=1; $total_mb / 1024" | bc)
+  rounded_gb=$(printf "%.0f" "$total_gb") # Pembulatan ke bilangan bulat
+  if [ "$(echo "$total_gb == $rounded_gb" | bc)" -eq 1 ]; then
+    # Jika tidak ada pecahan, tampilkan sebagai bilangan bulat
+    echo "$rounded_gb GB"
+  else
+    # Jika ada pecahan, tampilkan dengan satu desimal
+    echo "$total_gb GB"
+  fi
+fi
