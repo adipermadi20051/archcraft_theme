fef99120d24796a86e318f1f83eaec9e929df7d9
dprmd <adipr671@gmail.com>
2024-08-28 22:02:36 +0700

    change rofi-music

--- dprmd.py
+++ dprmd.py
@@ -16,9 +16,11 @@ def main():
     option = int(input("---> "))
     if option == 1:
         system(f"cp -r ~/.config/openbox/themes/dprmd {my_config_location}");
+        system(f"cp -r ~/.config/openbox/scripts/rofi-music {my_config_location}/addons")
         return "Copy File From Openbox Themes to DPRMD";
     if option == 2:
         system(f"cp -r {my_config_location}/dprmd ~/.config/openbox/themes/");
+        system(f"cp {my_config_location}/addons/rofi-music ~/.config/openbox/scripts")
         return "Apply File From DPRMD to Openbox Themes"
 
 print(main())
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 clear
-cp -r dprmd ~/.config/openbox/themes/
-cp -a fonts/. ~/.fonts/
-
+cp -r ./dprmd ~/.config/openbox/themes/
+cp -a ./fonts/. ~/.fonts/
+cp ./addons/rofi-music ~/.config/openbox/scripts/rofi-music
